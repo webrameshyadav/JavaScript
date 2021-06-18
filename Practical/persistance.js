6ead54ad0dcf774b96ce9886afe4c104e2974c76
webrameshyadav <83348002+webrameshyadav@users.noreply.github.com>
2021-06-18 23:24:56 +0530

    added

--- Practical/persistance.js
+++ Practical/persistance.js
@@ -60,7 +60,7 @@ console.log(1 + +"2" + "2");//32
 console.log(1 + -"1" + "2");//NAN2
 console.log(+"1" + "1" + "2");//112
 console.log( "A" - "B" + "2");//NAN2
-console.log( "A" - "B" + 2);//NAN2
+console.log( "A" - "B" + 2);//NAN
 
 console.table('------------'); 
 (function(){
@@ -80,3 +80,5 @@ console.table('------------');
        });
        console.log(newarr.flat(1));
         //["Max", "Jack", "Sam", "Alex", "Megan", "Mason", "Cameron", "Kaylin"]
+
+    console.log( true && false && false) ;   
